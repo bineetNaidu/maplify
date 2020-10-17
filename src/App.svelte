ae08693f54fff035bd6852fdfee2f9c3ee2360af
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-17 14:50:05 +0530

    add base form

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+  import Form from './Form.svelte';
 
   mapboxgl.accessToken = MAPBOX_TOKEN;
   const map = new mapboxgl.Map({
@@ -8,6 +9,8 @@
     center: [21.500353252696783, 35.067585836584584], // starting position [lng, lat]
     zoom: 1.5, // starting zoom
   });
+
+  $: showForm = true;
 
   // Add zoom and rotation controls to the map.
   map.addControl(new mapboxgl.NavigationControl());
@@ -18,3 +21,34 @@
     new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
   });
 </script>
+
+<style>
+  .backdrop {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.7);
+  }
+  .backdrop-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 15% 30%;
+    background: #67809645;
+    padding: 25px;
+    border-radius: 15px;
+    transform: translateX(0%);
+  }
+  .backdrop.show {
+    display: none;
+    pointer-events: none;
+  }
+</style>
+
+<div class="backdrop" class:show={showForm}>
+  <div class="backdrop-body">
+    <Form />
+  </div>
+</div>
---
+++ src/Form.svelte
@@ -0,0 +1,38 @@
+<script>
+  let desc;
+  let city;
+  export let lng;
+  export let lat;
+
+  const handleSubmit = () => console.log({ desc, city, lng, lat });
+</script>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  input {
+    font-family: inherit;
+    font-size: inherit;
+    -webkit-padding: 0.4em 0;
+    padding: 0.4em;
+    margin: 0 0 0.5em 0;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    color: white;
+    border-radius: 2px;
+    background: #347f6640;
+  }
+  button {
+    box-sizing: border-box;
+    color: white;
+    background: #347f6640;
+  }
+</style>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" bind:value={city} placeholder="City" />
+  <input type="text" bind:value={desc} placeholder="Short Desc..." />
+  <button type="submit" on:click|preventDefault={handleSubmit}>Add</button>
+</form>
