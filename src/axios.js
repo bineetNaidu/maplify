3e9d3f252dd95b3d18f3a3a34c90af5b31a0f27b
bineetNaidu <66471461+bineetNaidu@users.noreply.github.com>
2020-12-10 09:48:38 +0530

    Updated Prod URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'https://my-api-server-en.herokuapp.com/api/v1/',
+  baseURL: 'https://api-bineetnaidu-io.herokuapp.com/api/v1/',
 });
 
 export default axios;
